550d82e6cd696574b6e96da0ab7cd3d63880d81c
Salah-XD <hudeensala948@gmail.com>
2022-12-14 14:42:55 +0530

    delivery .js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Nav from "./components/Nav";
 import Checkout from "./components/Checkout";
 import Login from "./Login";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Delivery from "./components/delivery";
 
 function App() {
   return (
@@ -28,6 +29,7 @@ function App() {
           element={
             <>
               <Nav />
+              <Delivery />
               <Home />
             </>
           }
